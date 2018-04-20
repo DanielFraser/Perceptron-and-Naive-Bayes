d13676ff454e69936d907d4ac1f51ab7e1130a48
IHateLogicErrors <dukeydk@gmail.com>
2018-04-20 09:42:47 -0400

    Added constructor for perceptron

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,11 +1,22 @@
-import java.util.Hashtable;
+import java.util.*;
 
 public class Perceptron {
 
-    Hashtable weights = new Hashtable();
+    private Map<String, Map<String, Double>> weights = new HashMap<>();
+
+    public Perceptron(ArrayList<String> classes, Set<String> features)
+    {
+        Random rand = new Random();
+        Map<String, Double> temp = new HashMap<>();
+        for(String feat : features)
+            temp.put(feat, rand.nextDouble()); //generate random weights for all features
+
+        for(String s : classes)
+            weights.put(s, temp);
+    }
 
     public void train(int[][][] images, int[] output)
     {
-        
+        weights.keySet();
     }
 }
