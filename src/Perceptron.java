dfb47c92dc3bd59f597712ecf4d008ae6702c3c6
Daniel R Fraser <dukeydk@gmail.com>
2018-04-25 11:19:05 -0400

    Added w0 to weights

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -1,16 +1,20 @@
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Perceptron {
 
     private Map<Integer, Map<String, Double>> weights = new HashMap<>();
     private ArrayList<String> features;
-    private ArrayList<Integer> classes;
-    final double learningRate = 0.2;
+    private List<Integer> classes;
+    private int[] w0;
+    private final double learningRate = 0.2;
 
-    public Perceptron(ArrayList<Integer> classes, Set<String> features) {
-        this.classes = classes;
-        this.features.addAll(features);
+    public Perceptron(List<Map<String, Integer>> featuresList, int max) {
+        this.w0 = new int[classes.size()];
+        this.classes = IntStream.rangeClosed(0, max).boxed().collect(Collectors.toList());;
+        this.features.addAll(featuresList.get(0).keySet());
         Random rand = new Random();
         Map<String, Double> temp = new HashMap<>();
         for (String feat : features)
@@ -30,7 +34,7 @@ public class Perceptron {
     }
 
     private void predictTrain(Map<String, Integer> featuresList, int answer) {
-        boolean val = false;
+        boolean val;
         for (Integer aClass : classes) {
             val = predictClass(aClass, featuresList) >= 0;
             if (aClass == answer)
@@ -46,6 +50,7 @@ public class Perceptron {
         int curTotal;
         for (int i = 0; i < classes.size(); i++) {
             curTotal = predictClass(classes.get(i), featuresList);
+            curTotal += w0[i];
             if (curTotal > max[0]) {
                 max[0] = curTotal;
                 max[1] = i;
@@ -54,17 +59,18 @@ public class Perceptron {
         return max[1];
     }
 
-    private int predictClass(Integer classStr, Map<String, Integer> featureMap) {
+    private int predictClass(Integer classInt, Map<String, Integer> featureMap) {
         int total = 0;
         for (String feature : features) {
-            total += featureMap.get(feature) * (weights.get(classStr)).get(feature);
+            total += featureMap.get(feature) * (weights.get(classInt)).get(feature);
         }
-        return total;
+        return total+w0[classInt];
     }
 
     private void updateWeights(int classA, int error) {
         for (int i = 0; i < features.size(); i++) {
             weights.get(classA).put(features.get(i), weights.get(classA).get(i) + (learningRate*error));
+            w0[classA] += error > 0 ? 1 : -1;
         }
     }
 }
