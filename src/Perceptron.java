f98892f8a364af677992d4618752bc1305d8e990
Daniel R Fraser <dukeydk@gmail.com>
2018-04-25 10:54:57 -0400

    Perceptron is nearly done.

--- src/Perceptron.java
+++ src/Perceptron.java
@@ -4,9 +4,11 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Perceptron {
 
     private Map<Integer, Map<String, Double>> weights = new HashMap<>();
-    private ArrayList<String> classes, features;
+    private ArrayList<String> features;
+    private ArrayList<Integer> classes;
+    final double learningRate = 0.2;
 
-    public Perceptron(ArrayList<String> classes, Set<String> features) {
+    public Perceptron(ArrayList<Integer> classes, Set<String> features) {
         this.classes = classes;
         this.features.addAll(features);
         Random rand = new Random();
@@ -14,44 +16,55 @@ public class Perceptron {
         for (String feat : features)
             temp.put(feat, ThreadLocalRandom.current().nextDouble(-1, 1)); //generate random weights for all features
 
-        for (String s : classes)
-            weights.put(Integer.valueOf(s), temp);
+        for (Integer s : classes)
+            weights.put(s, temp);
     }
 
     public void train(List<Map<String, Integer>> featuresList, int[] answers) {
-        Map<String, Integer> curFeats;
-        int error;
-        double curVal = 0;
+
         for (int a = 0; a < 100; a++) {
             for (int i = 0; i < featuresList.size(); i++) {
-                curVal = 0;
-                curFeats = featuresList.get(i);
-                for (String feature : features) {
-                    curVal += curFeats.get(feature) * (weights.get(answers[i])).get(feature);
-                }
-                error = curVal >= 0 ? 1 : 0;
-
-                updateWeights(error);
+                predictTrain(featuresList.get(i), answers[i]);
             }
         }
     }
 
-    public boolean predict(Map<String, Integer> featuresList, int answer) {
-        for (String aClass : classes) {
-            predictClass(aClass, featuresList, answer);
+    private void predictTrain(Map<String, Integer> featuresList, int answer) {
+        boolean val = false;
+        for (Integer aClass : classes) {
+            val = predictClass(aClass, featuresList) >= 0;
+            if (aClass == answer)
+                if (!val)
+                    updateWeights(aClass, Math.abs(aClass - answer));
+                else if (val)
+                    updateWeights(aClass, -Math.abs(aClass - answer));
         }
-        return false;
     }
 
-    private void predictClass(String classStr, Map<String, Integer> featureMap, int answer) {
+    public int predict(Map<String, Integer> featuresList) {
+        int max[] = {Integer.MIN_VALUE, 0};
+        int curTotal;
+        for (int i = 0; i < classes.size(); i++) {
+            curTotal = predictClass(classes.get(i), featuresList);
+            if (curTotal > max[0]) {
+                max[0] = curTotal;
+                max[1] = i;
+            }
+        }
+        return max[1];
+    }
+
+    private int predictClass(Integer classStr, Map<String, Integer> featureMap) {
         int total = 0;
         for (String feature : features) {
-            total += featureMap.get(feature) * (weights.get(answer)).get(feature);
+            total += featureMap.get(feature) * (weights.get(classStr)).get(feature);
         }
-        //if()
+        return total;
     }
 
-    private void updateWeights(int error) {
-
+    private void updateWeights(int classA, int error) {
+        for (int i = 0; i < features.size(); i++) {
+            weights.get(classA).put(features.get(i), weights.get(classA).get(i) + (learningRate*error));
+        }
     }
 }
